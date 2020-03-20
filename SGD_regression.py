0e06d3bdc44765cab7e5c2cc477084f57ede46ed
Lucaserf <luca.serfilippi97@gmail.com>
2020-03-20 12:00:35 +0100

    added momentum

--- SGD_regression.py
+++ SGD_regression.py
@@ -13,24 +13,26 @@ def custom_function(X):
     return 10+8*X+5*X**2
 
 
-def stochastic_gradient_descent(X, y, theta, batch_dimension=10, learning_rate=0.00001, iterations=1000):
+def stochastic_gradient_descent(X, y, theta, batch_dimension=10, learning_rate=0.00001, iterations=1000, momentum=0):
     error_list = []
+    adjust_list = [theta*0]
     x_mat = np.array([X**i for i in range(len(theta))]).T
     for it in range(iterations):
         batch = np.random.randint(len(X), size=batch_dimension)
-        X_batch = x_mat[batch,:]
-        prediction = np.dot(X_batch,theta.T)
+        X_batch = x_mat[batch, :]
+        prediction = np.dot(X_batch, theta.T)
         error_list.append(np.sum(prediction - y[batch]))
-        adjust = (1/batch_dimension)*learning_rate*(X_batch.T.dot((prediction - y[batch])))
-        #print(adjust)
+        adjust = (1/batch_dimension)*learning_rate*(X_batch.T.dot((prediction - y[batch]))) + momentum*adjust_list[it-1]
         theta = theta - adjust
-    return theta, error_list
+        adjust_list.append(adjust)
+    return theta, error_list, adjust_list
 
 
 
 iter = 100
 X, y = points_generator(50, custom_function, 5e2)
-theta, error_list = stochastic_gradient_descent(X, y, np.array([2,1,3]), batch_dimension=20, learning_rate=1e-6, iterations=iter)
+theta, error_list, adjust_list = stochastic_gradient_descent(X, y, np.array([2, 1, 3]), batch_dimension=20, learning_rate=1e-8, iterations=iter, momentum=0.9)
+print(adjust_list)
 print(theta)
 
 
