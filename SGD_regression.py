845e2a92ad98827aaf81aff7cc04dc24c0519791
DanieleVeri <daniele.veri.96@gmail.com>
2020-03-21 16:46:10 +0100

    working linear regression

--- SGD_regression.py
+++ SGD_regression.py
@@ -1,41 +1,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-def points_generator(quantity, function, amplitude_noise):
+def points_generator(quantity, function, precision, amplitude_noise):
     np.random.seed(10)
-    X = np.arange(quantity)
-    noise = (np.random.rand(quantity)-0.5) * 2*amplitude_noise
-    y = function(X) + noise
+    limit = quantity/(2*(10**-precision))
+    #X = np.arange(-limit, limit, 1*10**precision)
+    X = np.arange(0, 2*limit, 1 * 10 ** precision)
+    y = function(X) + (np.random.rand(quantity)-0.5)*2*amplitude_noise
     return X, y
 
-
 def custom_function(x):
-    return 100 + 8*x
+    return 8+8*x
 
+def loss_fn(prediction, y):
+    return np.sum((prediction - y)**2)
 
 def stochastic_gradient_descent(x, y, coefficients, batch_dimension, learning_rate, iterations):
     error_list = []
+    x_mat = np.array([x**i for i in range(len(coefficients))]).T
     for it in range(iterations):
         batch = np.random.randint(len(x), size=batch_dimension)
-        x_batch = x[batch]
-        #print(x_batch)
-        prediction = coefficients[0] + coefficients[1] * x_batch
-        print(y[batch])
-        print(prediction)
-        input()
-        error_list.append(np.sum(prediction - y[batch]))
-
-        adjust = (1/batch_dimension) * learning_rate * (x_batch.T.dot((prediction - y[batch])))
-        #print(adjust)
+        X_batch = x_mat[batch, :]
+        prediction = np.dot(X_batch, coefficients.T)
+        error = loss_fn(prediction, y[batch])
+        error_list.append(error)
+        gradient = (X_batch.T.dot((prediction - y[batch])))
+        adjust = (1/batch_dimension)*learning_rate * gradient
         coefficients = coefficients - adjust
     return coefficients, error_list
 
 
-iterations = 10000
-x, y = points_generator(50, custom_function, 10)
+iterations = 200000
+x, y = points_generator(100, custom_function, 0, 10)
 coefficients, error_list = stochastic_gradient_descent(x, y, np.array([0, 0]),
-    batch_dimension=20, learning_rate=1e-6, iterations=iterations)
+    batch_dimension=20, learning_rate=1e-4, iterations=iterations)
 print(coefficients)
 
 x_mat = np.ones(len(x), dtype=int)
