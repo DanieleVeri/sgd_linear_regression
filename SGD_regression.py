58856b6cdedbc29580df16660eb2626b857e41e3
DanieleVeri <daniele.veri.96@gmail.com>
2020-03-21 17:56:46 +0100

    ocean dataset

--- SGD_regression.py
+++ SGD_regression.py
@@ -1,16 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
-def points_generator(quantity, function, precision, amplitude_noise):
-    np.random.seed(10)
-    limit = quantity/(2*(10**-precision))
-    #X = np.arange(-limit, limit, 1*10**precision)
-    X = np.arange(0, 2*limit, 1 * 10 ** precision)
-    y = function(X) + (np.random.rand(quantity)-0.5)*2*amplitude_noise
-    return X, y
-
-def custom_function(x):
-    return 8+8*x
+def load_ocean_data():
+    df = pd.read_csv('ocean.csv')
+    return df[['R_SALINITY', 'R_TEMP']]
 
 def loss_fn(prediction, y):
     return np.sum((prediction - y)**2)
@@ -30,18 +24,27 @@ def stochastic_gradient_descent(x, y, coefficients, batch_dimension, learning_ra
     return coefficients, error_list
 
 
-iterations = 200000
-x, y = points_generator(100, custom_function, 0, 10)
-coefficients, error_list = stochastic_gradient_descent(x, y, np.array([0, 0]),
-    batch_dimension=20, learning_rate=1e-4, iterations=iterations)
-print(coefficients)
+np.random.seed(10)
+iterations = 20000
 
-x_mat = np.ones(len(x), dtype=int)
-for i in range(1, len(coefficients)):
-    x_mat = np.vstack((x_mat, x ** i))
-x_mat = x_mat.T
-plt.plot(x, np.dot(x_mat, coefficients.T))
+data = load_ocean_data()
+data = data.dropna()
+#plt.plot(data['R_TEMP'], data['R_SALINITY'], ".")
+#plt.show()
+
+x, y = np.array(data['R_TEMP']), np.array(data['R_SALINITY'])
+batch_size = int(len(x) / 1000)
+
+#x, y = points_generator(100, custom_function, 0, 10)
+coefficients, error_list = stochastic_gradient_descent(x, y, np.array([0, 0]),
+    batch_dimension=batch_size, learning_rate=1e-3, iterations=iterations)
+print(coefficients)
+print(error_list[iterations-1])
+
+x_mat = np.array([x**i for i in range(len(coefficients))]).T
 plt.plot(x, y, '.')
+plt.plot(x, np.dot(x_mat, coefficients.T))
+
 plt.figure(2)
 plt.plot(np.arange(iterations), error_list)
 plt.show()
