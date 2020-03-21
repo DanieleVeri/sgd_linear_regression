703546907bbe3571369fce35bb679c451bd61e9e
Lucaserf <luca.serfilippi97@gmail.com>
2020-03-21 19:01:07 +0100

    fix warning e x_mat multivariate

--- SGD_regression.py
+++ SGD_regression.py
@@ -2,16 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 def load_ocean_data():
-    df = pd.read_csv('ocean.csv')
+    df = pd.read_csv('ocean.csv', low_memory=False)
     return df[['R_SALINITY', 'R_TEMP']]
+
 
 def loss_fn(prediction, y):
     return np.sum((prediction - y)**2)
 
-def stochastic_gradient_descent(x, y, coefficients, batch_dimension, learning_rate, iterations):
+
+def stochastic_gradient_descent(x, y, batch_dimension, learning_rate, iterations):
     error_list = []
-    x_mat = np.array([x**i for i in range(len(coefficients))]).T
+    x_mat = np.concatenate((np.ones((x.shape[0], 1), dtype=int), x), axis=1)
+    coefficients = np.zeros((1+x.shape[1], ), dtype=int)
     for it in range(iterations):
         batch = np.random.randint(len(x), size=batch_dimension)
         X_batch = x_mat[batch, :]
@@ -29,20 +33,16 @@ iterations = 20000
 
 data = load_ocean_data()
 data = data.dropna()
-#plt.plot(data['R_TEMP'], data['R_SALINITY'], ".")
-#plt.show()
-
-x, y = np.array(data['R_TEMP']), np.array(data['R_SALINITY'])
+x, y = np.array(data[['R_TEMP']], dtype=float), np.array(data['R_SALINITY'], dtype=float)
 batch_size = int(len(x) / 1000)
 
-#x, y = points_generator(100, custom_function, 0, 10)
-coefficients, error_list = stochastic_gradient_descent(x, y, np.array([0, 0]),
-    batch_dimension=batch_size, learning_rate=1e-3, iterations=iterations)
+coefficients, error_list = stochastic_gradient_descent(x, y, batch_dimension=batch_size,
+                                                       learning_rate=1e-3, iterations=iterations)
 print(coefficients)
 print(error_list[iterations-1])
 
-x_mat = np.array([x**i for i in range(len(coefficients))]).T
-plt.plot(x, y, '.')
+x_mat = np.concatenate((np.ones((x.shape[0], 1), dtype=int), x), axis=1)
+plt.plot(x, y.T, '.')
 plt.plot(x, np.dot(x_mat, coefficients.T))
 
 plt.figure(2)
